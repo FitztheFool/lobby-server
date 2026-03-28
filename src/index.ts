7a593601e2831c0dd3182d90ed407ed84812eb9f
Your Name <foolthefitz@gmail.com>
2026-03-28 09:06:58 +0100

    shutdown

--- src/index.ts
+++ src/index.ts
@@ -601,3 +601,7 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log("[LOBBY] realtime listening on", PORT));
+
+const shutdown = () => server.close(() => process.exit(0));
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
